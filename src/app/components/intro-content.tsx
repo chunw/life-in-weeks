0a718473ceded1b9489c1d97f830cceae538a455
Chun Wang <chunwang.me@gmail.com>
2026-03-04 01:46:10 -0500

    update files

--- src/app/components/intro-content.tsx
+++ src/app/components/intro-content.tsx
@@ -6,7 +6,7 @@ export function IntroContent() {
   return (
     <div className="intro-content">
       <div className="mt-3">
-        <p>This is <a href="https://www.chunwang.art" target='_blank' rel='noopener noreferrer'>Chun Wang</a>&apos;s life in weeks (<a target="_blank" rel="noopener noreferrer" href="https://www.chunwang.art/assets/chunwang-resume.pdf">CV</a>). Inspired by <a target="_blank" rel="noopener noreferrer" href="https://waitbutwhy.com/2014/05/life-weeks.html">Wait But Why</a>.</p>
+        <p>This is <a href="https://www.chunwang.art" target='_blank' rel='noopener noreferrer'>Chun Wang</a>&apos;s life in weeks (<a target="_blank" rel="noopener noreferrer" href="https://www.chunwang.art/assets/resume.pdf">CV</a>). Inspired by <a target="_blank" rel="noopener noreferrer" href="https://waitbutwhy.com/2014/05/life-weeks.html">Wait But Why</a>.</p>
 
         {/* <p style={{ fontSize: '0.9em', color: '#666', marginTop: '0.75rem' }}>
           🎨 <strong>Colors:</strong> Background colors change at major life milestones, marking different life phases.<br/>
